2d4605cf773c4b008df046bb5efe9789b89d23e7
Pierre  LUISI <pluisi@maestro-1039.maestro.pasteur.fr>
2021-08-30 22:48:08 +0200

    plink run with image

--- 1_shapeIT.sh
+++ 1_shapeIT.sh
@@ -1,9 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+
+
+
+SinguExec="singularity exec --home $HOME:/home/$USER --bind /pasteur "
+	-profile singularity,test_stresstest_human \
+
+folderImg=/pasteur/zeus/projets/p02/Hotpaleo/common_data/VMs/singularity/
+plinkImage=$folderImg/biocontainers-plink1.9-v1.90b6.6-181012-1-deb_cv1.img
+
+module load singularity
+module load java
 
 
 
 rootRef=/Volumes/MARIOLOLO/Data/1KG_Haplotypes/Phase3/
-folder=/pasteur/zeus/projets/p02/Hotpaleo/pierre/Projects/RAICES//fineStructure/
+
+folderScript=/pasteur/zeus/projets/p02/Hotpaleo/pierre/Scripts/RAICES/
+folder=/pasteur/zeus/projets/p02/Hotpaleo/pierre/Projects/RAICES/
+folderIN=$folder/StartFilteredData/
+
 mkdir $folder
 cd $folder
 mkdir shapeITphased
@@ -31,8 +46,8 @@ for chr in 22
 do
 	if [ ! -e $folder/shapeITphased/Genotipos_Raices.Plink.Autosomal.HGDP_1KG_SGDP_REDUCED.MAF$MAF.GENO$GENO.MIND$MIND.chr$chr"_alignedRef_phased.haps" ]
 	then
-		~/src/plink1.9/plink --bfile $folder/Genotipos_Raices.Plink.Autosomal.HGDP_1KG_SGDP_REDUCED --chr $chr --maf $MAF --geno $GENO --mind $MIND --make-bed --out $folder/shapeITphased/Genotipos_Raices.Plink.Autosomal.HGDP_1KG_SGDP_REDUCED.MAF$MAF.GENO$GENO.MIND$MIND.chr$chr
-		perl /Users/pierrespc/Documents/PostDoc/scripts/Tools/shapeIT/Prephase/1_runShapeIT.pl $folder/shapeITphased Genotipos_Raices.Plink.Autosomal.HGDP_1KG_SGDP_REDUCED.MAF$MAF.GENO$GENO.MIND$MIND.chr$chr $MIND $GENO $MAF $rootRef/genetic_map_chr$chr"_combined_b37.txt" $rootRef/1000GP_Phase3_chr$chr".hap.gz" $rootRef/1000GP_Phase3_chr$chr".legend.gz" $rootRef/1000GP_Phase3.sample $folder/shapeITphased $state $window $thread $burn $prune $main $effSize
+		$SinguExec $plinkImage plink1.9 --bfile $folderIN/Genotipos_Raices.Plink.Autosomal.HGDP_1KG_SGDP_REDUCED --chr $chr --maf $MAF --geno $GENO --mind $MIND --make-bed --out $folder/shapeITphased/Genotipos_Raices.Plink.Autosomal.HGDP_1KG_SGDP_REDUCED.MAF$MAF.GENO$GENO.MIND$MIND.chr$chr
+		echo perl $folderScript/1_runShapeIT.pl $folder/shapeITphased Genotipos_Raices.Plink.Autosomal.HGDP_1KG_SGDP_REDUCED.MAF$MAF.GENO$GENO.MIND$MIND.chr$chr $MIND $GENO $MAF $rootRef/genetic_map_chr$chr"_combined_b37.txt" $rootRef/1000GP_Phase3_chr$chr".hap.gz" $rootRef/1000GP_Phase3_chr$chr".legend.gz" $rootRef/1000GP_Phase3.sample $folder/shapeITphased $state $window $thread $burn $prune $main $effSize
 	else
 		echo $folder/shapeITphased/Genotipos_Raices.Plink.Autosomal.HGDP_1KG_SGDP_REDUCED.MAF$MAF.GENO$GENO.MIND$MIND.chr$chr"_alignedRef_phased.haps" already generated
 	fi
